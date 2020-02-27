f6b7e43a82e045a9e8ec3e9818689504d4b8ba09
Michał Sitko <pierwszy1@gmail.com>
2020-02-27 10:51:21 +0100

    Take version from sbt version

---
+++ ci/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Exit on any failure
+set -e
+
+java -version
+sbt assembly
+VERSION=`sbt version | tail -n 1 | awk '{print $2}'`
+native-image --verbose --static --no-fallback -jar "target/scala-2.13/teleport-scala-assembly-$VERSION.jar" teleport-scala
