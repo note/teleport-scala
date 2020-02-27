d99b518e5f07eaab9afcabbacbf6177180785f4b
Michał Sitko <pierwszy1@gmail.com>
2020-02-27 14:56:36 +0100

    Update CI script

--- ci/script.sh
+++ ci/script.sh
@@ -7,3 +7,6 @@ java -version
 sbt assembly
 VERSION=`sbt version | tail -n 1 | awk '{print $2}'`
 native-image --verbose --static --no-fallback -jar "target/scala-2.13/teleport-scala-assembly-$VERSION.jar" teleport-scala
+
+# smoke-test uses teleport-scala built above
+./smoke-test.sh
