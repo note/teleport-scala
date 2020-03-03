c0a790d494bee9b468c0f53dbb934557f7719d75
Michał Sitko <pierwszy1@gmail.com>
2020-03-03 17:54:20 +0100

    Fix for travis release

--- ci/script.sh
+++ ci/script.sh
@@ -10,9 +10,18 @@ VERSION=`sbt version | tail -n 1 | awk '{print $2}'`
 if [ $TRAVIS_OS_NAME = windows ]; then
   find target -iname "*.jar" -exec cp {} teleport-scala.jar \;
   ci/windows.bat
+elif [ $TRAVIS_OS_NAME = osx ]; then
+  native-image --verbose --no-fallback -jar "target/scala-2.13/teleport-scala-assembly-$VERSION.jar" teleport-scala	
 else
   native-image --verbose --static --no-fallback -jar "target/scala-2.13/teleport-scala-assembly-$VERSION.jar" teleport-scala
 fi
 
 # smoke-test uses teleport-scala built above
 ./smoke-test.sh
+
+
+if [ $TRAVIS_OS_NAME = windows ]; then
+  export RELEASED_PACKAGE_FILENAME="teleport-scala.exe"
+else
+  export RELEASED_PACKAGE_FILENAME="teleport-scala-$TRAVIS_OS_NAME"
+fi
