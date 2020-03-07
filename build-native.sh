4553ddb3212808f463c74d3115643b65276b9a38
Michał Sitko <pierwszy1@gmail.com>
2020-03-07 23:59:29 +0100

    Improve build-native.sh

--- build-native.sh
+++ build-native.sh
@@ -3,5 +3,6 @@
 # Exit on any failure
 set -e
 
+sbt assembly
 find target -iname "*.jar" -exec cp {} teleport-scala.jar \;
-native-image --verbose --static --no-fallback  -jar teleport-scala.jar teleport-scala
+native-image --verbose -H:+ReportExceptionStackTraces --static --no-fallback -jar teleport-scala.jar teleport-scala
